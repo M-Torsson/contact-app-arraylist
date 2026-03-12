164cb6f024b1cd65916a3cd0a95a3e542ba651f1
Muthana Torsson <muthana.tursson@gmail.com>
2026-03-12 12:41:42 +0200

    Implement contact management functionality

--- src/main/java/Lexcon/Main.java
+++ src/main/java/Lexcon/Main.java
@@ -1,17 +1,44 @@
 package Lexcon;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        ContactDAO dao = new ContactDAO();
+        int choice = -1;
+        while (choice != 0) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("\nContact App");
+            System.out.println("1 Add Contact");
+            System.out.println("2 Search Contact");
+            System.out.println("3 Show All");
+            System.out.println("0 Exit");
+            System.out.print("Choose: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            if (choice == 1) {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Mobile: ");
+                String mobile = sc.nextLine();
+                dao.addContact(name, mobile);
+            }
+            else if (choice == 2) {
+                System.out.print("Enter name: ");
+                String name = sc.nextLine();
+                dao.searchByName(name);
+            }
+            else if (choice == 3) {
+                dao.showAllContacts();
+            }
+            else if (choice == 0) {
+                System.out.println("Bye");
+            }
+            else {
+                System.out.println("Wrong choice");
+            }
         }
+        sc.close();
     }
 }
